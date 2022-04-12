451ad89d4bd287e21130411f3468284bd4a19af8
Randy1005 <randychang.10@gmail.com>
2022-04-12 00:03:08 -0600

    main program modified

--- main/main.cpp
+++ main/main.cpp
@@ -7,21 +7,26 @@ int main(int argc, char* argv[]) {
     std::cerr << "Usage: ./QSat input_dimacs_file.cnf" << std::endl;
     std::exit(EXIT_FAILURE);
   }
-
+ 
   qsat::Solver solver;
+  
+  std::chrono::steady_clock::time_point start_time, end_time; 
   solver.read_dimacs(argv[1]);
 
-  std::chrono::steady_clock::time_point start_time, end_time; 
   start_time = std::chrono::steady_clock::now(); 
   if (solver.solve()) {
     end_time = std::chrono::steady_clock::now();
     solver.dump(std::cout);
+  } else {
+
   }
 
-  std::chrono::duration<double> elapsed_time = end_time - start_time;
+  std::chrono::duration<double, std::milli> elapsed_time = end_time - start_time;  
   std::cout << "Run time: " 
-            <<  elapsed_time.count() * 1000.0f
+            << elapsed_time.count()
             << " ms\n";
+
+
 
 
   return 0;
